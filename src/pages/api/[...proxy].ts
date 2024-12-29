7232582636e25a26eaa423fc37751cc711613894
miaaaa0a <miaaaa0a@proton.me>
2024-12-29 21:04:45 +0300

    replace fetch with axios

--- src/pages/api/[...proxy].ts
+++ src/pages/api/[...proxy].ts
@@ -1,4 +1,5 @@
 import type { APIRoute } from "astro";
+import axios from 'axios';
 
 const getProxyUrl = (request: Request) => {
   const proxyUrl = new URL("http://127.0.0.1:8000/");
@@ -9,6 +10,6 @@ const getProxyUrl = (request: Request) => {
 
 export const ALL: APIRoute = async ({ request }) => {
   const proxyUrl = getProxyUrl(request);
-  const response = await fetch(proxyUrl.href, request);
-  return new Response(response.body);
+  const response = await axios.get(proxyUrl.href, {params: request, responseType: 'text'});
+  return new Response(response.data);
 };
